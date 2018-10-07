67b7775e66c7a53f047498ddffc5a90c5b281984
Mitchell Larson <larsonma@msoe.edu>
2018-10-07 00:18:11 -0500

    Initial implementaion of channel monitor for STM32 board

---
+++ inc/Manchester_State.h
@@ -0,0 +1,21 @@
+/*
+ * Manchester_State.h
+ *
+ *  Created on: Oct 6, 2018
+ *      Author: larsonma
+ */
+
+#ifndef MANCHESTER_STATE_H_
+#define MANCHESTER_STATE_H_
+
+#include "interrupt_timer.h"
+#include "RX.h"
+
+#define ISPR0 (volatile uint32_t*) 0xE000E200
+
+enum STATES {IDLE, BUSY, COLLISION};
+
+extern void init_state();
+enum STATES getState();
+
+#endif /* MANCHESTER_STATE_H_ */
